710a9dacdf92f46b0c6c9da0dccce627cb062298
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-28 17:02:58 +0530

    Create prob8.cpp

---
+++ prob8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int  a, b, c ;
+cout <<"base of triangle";
+cin >>a;
+cout <<"height of triangle";
+cin >>b;
+c=(1/2)*a*b;
+cout << "required area" << c;
+return 0;
+}
