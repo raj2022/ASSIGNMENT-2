df9ea07ce977525b8cf1cda9302fa87d837cbc49
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-28 16:46:44 +0530

    Create prob6.cpp

---
+++ prob6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+int a, b;
+cout<<"square root of a";
+cin>>b;
+b=((double)sqrt(a));
+cout<<"square root"<<b;
+return 0;
+}
