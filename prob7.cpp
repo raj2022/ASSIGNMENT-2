673deaae99fac782f8d9ea3e51098de6e087dd87
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-28 16:53:51 +0530

    Update prob7.cpp

--- prob7.cpp
+++ prob7.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main() 
+{
+int  a, b, c;
+cout<<"two angles of triangle";
+cin>>a>>b;
+cout<<"c";
+c=180-(a+b);
+cout<<"required angle"<<c;
+	return 0;
+}
