f7cffa6edc36a71cbba58ca2edcc632a5cc1c475
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-28 17:14:41 +0530

    Create prob10.cpp

---
+++ prob10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int  a,b, c ,d ,e ;
+float Avg, per;
+
+cout <<"marks of five subjects";
+cin >>a>>b>>c>>d>>e;
+Avg=(a+b+c+d+e)/5;
+per=(a+b+c+d+e)/5;
+
+cout << "Average" << Avg;
+cout << "Percentage" << per;
+return 0;
+}
