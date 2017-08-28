5efc9035323bc97594bb228c89c63f5c2aaba0c3
raj2022 <31206135+raj2022@users.noreply.github.com>
2017-08-28 16:18:20 +0530

    Create prob4.cpp

---
+++ prob4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int days,weeks,years;
+cout<<"no. of days";
+cin>> days;
+years=days/365;
+weeks=days/7;
+cout<<" year is "<<years<<" and weeks are "<<weeks; 
+return 0;
+}
